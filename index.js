fbb933bd518434cadfa24021d676e9b1efcc29b6
Franklin Liu <franklin.liu@autodata.net>
2019-07-19 14:42:40 -0400

    production setup

--- index.js
+++ index.js
@@ -46,13 +46,6 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-// app.use(express.static(path.join(__dirname, "../client/build")));
-// /*React root*/
-// app.get("*", (req, res) => {
-// res.sendFile(path.join(__dirname + "../client/build/index.html"));
-// });
-
-
 app.use(express.static(__dirname + '/public'))
    .use(cors())
    .use(cookieParser());
@@ -159,6 +152,14 @@ app.get('/refresh_token', function(req, res) {
     }
   });
 });
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, function() {
   console.log(`Node cluster worker ${process.pid}: listening on port ${port}`);
